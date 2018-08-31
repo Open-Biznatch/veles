3777dbb4b796ac22e464c34edd7a6514d00d56e8
JustinPercy <justin@protonmail.com>
2018-08-31 00:41:41 -0500

    cleanup

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -211,7 +211,7 @@ void CMasternode::Check(bool forceCheck)
     if (!unitTest) {
         CValidationState state;
         CMutableTransaction tx = CMutableTransaction();
-        CTxOut vout = CTxOut((Params().MasternodeCollateral() * COIN, obfuScationPool.collateralPubKey);
+        CTxOut vout = CTxOut(Params().MasternodeCollateral() * COIN, obfuScationPool.collateralPubKey);
         tx.vin.push_back(vin);
         tx.vout.push_back(vout);
 
