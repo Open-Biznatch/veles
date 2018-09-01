43efb56d00648004484384f0ccb9d578a04675b7
JustinPercy <justin@protonmail.com>
2018-09-01 14:16:58 -0500

    Masternode Fixes

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -269,13 +269,13 @@ bool CActiveMasternode::Register(std::string strService, std::string strKeyMaste
     CService service = CService(strService);
     int mainnetDefaultPort = Params(CBaseChainParams::MAIN).GetDefaultPort();
     if (Params().NetworkID() == CBaseChainParams::MAIN) {
-        if (service.GetPort() != mainnetDefaultPort) {
-            errorMessage = strprintf("Invalid port: %u - only %d is supported on mainnet.", service.GetPort(), mainnetDefaultPort);
+        if (strService.GetPort() != mainnetDefaultPort) {
+            errorMessage = strprintf("Invalid port: %u - only %d is supported on mainnet.", strService.GetPort(), mainnetDefaultPort);
             LogPrintf("CActiveMasternode::ManageStatus() - not capable: %s\n", errorMessage);
             return false;
         }
-    } else if (service.GetPort() == mainnetDefaultPort) {
-        errorMessage = strprintf("Invalid port: %u - %d is only supported on mainnet.", service.GetPort(), mainnetDefaultPort);
+    } else if (strService.GetPort() == mainnetDefaultPort) {
+        errorMessage = strprintf("Invalid port: %u - %d is only supported on mainnet.", strService.GetPort(), mainnetDefaultPort);
         LogPrintf("CActiveMasternode::ManageStatus() - not capable: %s\n", errorMessage);
         return false;
     }
